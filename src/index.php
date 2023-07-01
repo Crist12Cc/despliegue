49864f52bcee5c8494f6108ecba0c50955b7f213
Cristian Comayagua <comayaguac@gmail.com>
2023-07-01 15:34:03 -0600

    config intellij

--- src/index.php
+++ src/index.php
@@ -3,9 +3,10 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Inicio</title>
+    <title>Despliegue</title>
   </head>
   <body>
-    <h1>Despliegue----CC17078****Cristian Comayagua</h1>
+    <h1>****Despliegue***</h1>
+    <p>----CC17078 Cristian Comayagua----</p>
   </body>
 </html>
